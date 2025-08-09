1265565857e2fb7a413247252df411ac11ddbfb4
Bruno Pereira da Silva <bunros52@gmail.com>
2025-08-09 12:29:36 -0300

    refactor: componentizing the left side menu

---
+++ src/components/LeftAside.tsx
@@ -0,0 +1,68 @@
+import { 
+    AiOutlineHome, 
+    AiOutlineSearch,
+    AiOutlineBell,
+    AiOutlineTag,
+    AiOutlineUser,
+    AiFillLock } from "react-icons/ai";
+import { BsThreeDots } from "react-icons/bs";
+import { GoPersonFill } from "react-icons/go";
+
+
+import logo from '../assets/LOGO_X.svg';
+
+const LeftAside = () => {
+
+    return (
+        <aside className='w-full max-w-[353px] px-12 pt-5 pb-14 text-white sticky left-0 top-0 h-screen'>
+                <img 
+                    src={logo} 
+                    alt="X Logo" 
+                    className="w-9"
+                />
+                <button className="flex items-center gap-7 transition-colors cursor-pointer hover:bg-neutral-900 p-3 rounded-full mt-10">
+                    <AiOutlineHome  className="w-9 h-9"/>
+                    <span className="text-white text-2xl">
+                        Página Inicial
+                    </span>
+                </button>
+                <button className="flex items-center gap-7 transition-colors cursor-pointer hover:bg-neutral-900 p-3 rounded-full mt-10">
+                    <AiOutlineSearch   className="w-9 h-9"/>
+                    <span className="text-white text-2xl">
+                        Explorar
+                    </span>
+                </button>
+                <button className="flex items-center gap-7 transition-colors cursor-pointer hover:bg-neutral-900 p-3 rounded-full mt-10">
+                    <AiOutlineBell   className="w-9 h-9"/>
+                    <span className="text-white text-2xl">
+                        Notifícações
+                    </span>
+                </button>
+                <button className="flex items-center gap-7 transition-colors cursor-pointer hover:bg-neutral-900 p-3 rounded-full mt-10">
+                    <AiOutlineTag  className="w-9 h-9"/>
+                    <span className="text-white text-2xl">
+                        Itens salvos
+                    </span>
+                </button>
+                <button className="flex items-center gap-7 transition-colors cursor-pointer hover:bg-neutral-900 p-3 rounded-full mt-10">
+                    <AiOutlineUser  className="w-7 h-7"/>
+                    <span className="text-white text-2xl">
+                        Perfil
+                    </span>
+                </button>
+                <div className="flex mt-10 cursor-pointer hover:bg-neutral-900 p-3 rounded-full">
+                    <span className="w-12 h-12 bg-neutral-400 rounded-full flex items-center justify-center text-neutral-700">
+                                <GoPersonFill className=" w-12 h-9"/>
+                    </span>
+                    <div className="ml-4">
+                        <h3 className="font-bold flex items-center text-white">
+                            Nome Sobr... <AiFillLock  className="ml-1"/> <BsThreeDots className="ml-1"/>
+                        </h3>
+                        <span className="text-neutral-500 mx-2">@conta</span>
+                    </div>
+                </div>
+            </aside>
+    )
+  }
+
+  export default LeftAside
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -7,20 +7,12 @@ import {
     AiOutlineCalendar, 
     AiOutlineSmile, 
     AiOutlineEnvironment, 
-    AiOutlineHeart, 
-    AiOutlineHome, 
-    AiOutlineSearch,
-    AiOutlineBell,
-    AiOutlineTag,
-    AiOutlineUser,
-    AiFillLock } from "react-icons/ai";
+    AiOutlineHeart, } from "react-icons/ai";
 import { IoChatbubbleEllipsesOutline, IoStatsChart, IoBookmarkOutline, IoBookmark    } from "react-icons/io5";
 import { FaRetweet } from "react-icons/fa6";
 import { MdOutlineFileUpload } from "react-icons/md";
-import { BsThreeDots } from "react-icons/bs";
 
-
-import logo from '../../assets/LOGO_X.svg';
+import LeftAside from "../../components/LeftAside";
 
 
 
@@ -32,54 +24,7 @@ const Home = () => {
 
     return (
         <div className=' h-full min-h-screen flex bg-black'>
-            <aside className='w-full max-w-[353px] px-12 pt-5 pb-14 text-white sticky left-0 top-0 h-screen'>
-                <img 
-                    src={logo} 
-                    alt="X Logo" 
-                    className="w-9"
-                />
-                <button className="flex items-center gap-7 transition-colors cursor-pointer hover:bg-neutral-900 p-3 rounded-full mt-10">
-                    <AiOutlineHome  className="w-9 h-9"/>
-                    <span className="text-white text-2xl">
-                        Página Inicial
-                    </span>
-                </button>
-                <button className="flex items-center gap-7 transition-colors cursor-pointer hover:bg-neutral-900 p-3 rounded-full mt-10">
-                    <AiOutlineSearch   className="w-9 h-9"/>
-                    <span className="text-white text-2xl">
-                        Explorar
-                    </span>
-                </button>
-                <button className="flex items-center gap-7 transition-colors cursor-pointer hover:bg-neutral-900 p-3 rounded-full mt-10">
-                    <AiOutlineBell   className="w-9 h-9"/>
-                    <span className="text-white text-2xl">
-                        Notifícações
-                    </span>
-                </button>
-                <button className="flex items-center gap-7 transition-colors cursor-pointer hover:bg-neutral-900 p-3 rounded-full mt-10">
-                    <AiOutlineTag  className="w-9 h-9"/>
-                    <span className="text-white text-2xl">
-                        Itens salvos
-                    </span>
-                </button>
-                <button className="flex items-center gap-7 transition-colors cursor-pointer hover:bg-neutral-900 p-3 rounded-full mt-10">
-                    <AiOutlineUser  className="w-7 h-7"/>
-                    <span className="text-white text-2xl">
-                        Perfil
-                    </span>
-                </button>
-                <div className="flex mt-10 cursor-pointer hover:bg-neutral-900 p-3 rounded-full">
-                    <span className="w-12 h-12 bg-neutral-400 rounded-full flex items-center justify-center text-neutral-700">
-                                <GoPersonFill className=" w-12 h-9"/>
-                    </span>
-                    <div className="ml-4">
-                        <h3 className="font-bold flex items-center text-white">
-                            Nome Sobr... <AiFillLock  className="ml-1"/> <BsThreeDots className="ml-1"/>
-                        </h3>
-                        <span className="text-neutral-500 mx-2">@conta</span>
-                    </div>
-                </div>
-            </aside>
+            <LeftAside/>
             <div className=" w-full border-x-1 border-neutral-700 relative">
                 <div className="grid grid-cols-2 w-full max-h-16 bg-black/70 text-neutral-500 font-bold place-items-center border-b-1 border-neutral-700  sticky top-0 left-0">
                     <button onClick={() => (setShowForYou(true), setShowFollowing(false))} className="cursor-pointer w-full h-full hover:bg-neutral-900 transition-colors">
