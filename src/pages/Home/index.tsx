6dc72e6b04de8a0fb4fddf0da2034ec6341d4f88
Bruno Pereira da Silva <bunros52@gmail.com>
2025-08-09 10:38:22 -0300

    feat: creating the tweet

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,13 @@
 import { useState } from "react";
 
 import { GoPersonFill } from "react-icons/go";
-import { AiOutlineFileImage, AiOutlineFileGif, AiOutlineCalendar, AiOutlineSmile, AiOutlineEnvironment    } from "react-icons/ai";
+import { AiOutlineFileImage, AiOutlineFileGif, AiOutlineCalendar, AiOutlineSmile, AiOutlineEnvironment, AiOutlineHeart     } from "react-icons/ai";
+import { IoChatbubbleEllipsesOutline, IoStatsChart, IoBookmarkOutline, IoBookmark    } from "react-icons/io5";
+import { FaRetweet } from "react-icons/fa6";
+import { MdOutlineFileUpload } from "react-icons/md";
+
+
+
 
 const Home = () => {
     const [showFollowing, setShowFollowing] = useState(true);
@@ -9,9 +15,9 @@ const Home = () => {
     const [text, setText] = useState('');
 
     return (
-        <div className=' h-screen flex'>
+        <div className=' h-full min-h-screen flex bg-black'>
             <h1 className='w-full max-w-[353px] bg-black'>Hello World 1</h1>
-            <div className=" w-full bg-black border-x-1 border-neutral-700">
+            <div className=" w-full border-x-1 border-neutral-700">
                 <div className="grid grid-cols-2 w-full max-h-16 bg-black text-neutral-500 font-bold place-items-center border-b-1 border-neutral-700">
                     <button onClick={() => (setShowForYou(true), setShowFollowing(false))} className="cursor-pointer w-full h-full">
                         <span className={`inline-block   ${showForYou ? 'border-b-4 border-primary_blue py-4 text-white' : ''}`}>
@@ -24,10 +30,10 @@ const Home = () => {
                         </span>
                     </button>
                 </div>
-                <div className="bg-black text-white p-5 text-2xl border-b-1 border-neutral-700">
+                <div className=" text-white p-5 text-2xl border-b-1 border-neutral-700">
                     <div className="flex gap-8 mb-5">
                         <span className="w-12 h-12 bg-neutral-400 rounded-full flex items-center justify-center text-neutral-700">
-                            <GoPersonFill className=" w-9 h-9"/>
+                            <GoPersonFill className=" w-12 h-9"/>
                         </span>
                         <textarea
                             value={text}
@@ -41,7 +47,6 @@ const Home = () => {
                             }}
                             placeholder="O que está acontecendo?"
                         />
-                        
                     </div>
                     <div className="flex justify-between">
                         <div className="flex items-center justify-between gap-7 ml-24 text-primary_blue">
@@ -54,6 +59,36 @@ const Home = () => {
                         <button className="bg-neutral-600 text-black text-xl font-bold py-2 px-3.5 rounded-full hover:bg-blue-600 transition-colors">
                             Postar
                         </button>
+                    </div>
+                </div>
+                <div className="text-white">
+                    <div className="p-5 w-full border-b-1 border-neutral-700">
+                        <div className="flex">
+                            <span className="w-12 h-12 bg-neutral-400 rounded-full flex items-center justify-center text-neutral-700">
+                                <GoPersonFill className=" w-12 h-9"/>
+                            </span>
+                            <div className="ml-4">
+                                <h3 className="font-bold">
+                                    Nome Sobrenome
+                                    <span className="text-neutral-500 mx-2">@conta</span>  <span className="text-neutral-500">22h</span>
+                                </h3>
+                                <h2 className="mb-7">
+                                    Pra quem não me conhece e passou a me seguir faz pouco tempo, eu me chamo Vinícius, sou designer freelance
+                                     (ainda cursando, mas quase completando) e eu levo MUITO a sério meu trabalho. Tiro meu pouco sustento disso, 
+                                     por vezes levo horas e me dedico ao máximo pra isso (+)
+                                </h2>
+                                <div className="flex items-center justify-between gap-7 text-neutral-500">
+                                    <IoChatbubbleEllipsesOutline className="cursor-pointer w-6 h-6"/>
+                                    <FaRetweet className="cursor-pointer w-6 h-6"/>
+                                    <AiOutlineHeart className="cursor-pointer w-6 h-6"/>
+                                    <IoStatsChart  className="cursor-pointer w-6 h-6"/>
+                                    <div className="flex items-center gap-2">
+                                        <IoBookmarkOutline className="cursor-pointer w-6 h-6"/>
+                                        <MdOutlineFileUpload className="cursor-pointer w-6 h-6"/>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
