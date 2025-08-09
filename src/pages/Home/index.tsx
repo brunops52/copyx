dbd57c9eb3841403f14c3e7e7e2bf9b9b74668b1
Bruno Pereira da Silva <bunros52@gmail.com>
2025-08-09 19:22:58 -0300

    refactor: componentizing the explore page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import LeftAside from "../../components/LeftAside";
 import RightAside from "../../components/RightAside";
 import TweetsPage from "../../components/TweetsPage";
-import { AiOutlineSearch } from "react-icons/ai";
+import ExplorePage from "../../components/ExplorePage";
 
 const Home = () => {
     const [toRender, setToRender] = useState("HOME");
@@ -19,14 +19,7 @@ const Home = () => {
                         return <TweetsPage />;
                     case "EXPLORE":
                         return (
-                            <div className=" w-full h-full min-h-screen border-x-1 border-neutral-700 relative text-white">
-                                <div className="px-6 py-2 pb-10 border-b-1 border-neutral-700">
-                                    <div className="border-1 border-neutral-700 rounded-full p-3 flex items-center gap-3">
-                                        <AiOutlineSearch   className="w-7 h-7 text-neutral-500"/>
-                                        <input placeholder="Buscar" type="text" className="focus:outline-0"/>
-                                    </div>
-                                </div>
-                            </div>
+                            <ExplorePage/>
                         )
                     case "b":
                         return <div className="flex-1 flex justify-center items-center text-white">Welcome to the Home Page</div>;
