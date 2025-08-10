dcf24c1cb6bd1d80c1feca87cfc6ab01372ba110
Bruno Pereira da Silva <bunros52@gmail.com>
2025-08-09 22:42:56 -0300

    feat: componentizing and rendering notifications dynamically

---
+++ src/components/Notification.tsx
@@ -0,0 +1,29 @@
+
+
+type NotificationProps = {
+img: string;
+content: string;
+time: string;
+};
+const Notification = ({ 
+    img, 
+    content,
+    time
+}: NotificationProps) => {
+
+
+    return (
+            
+            <div className="flex w-full p-5 gap-4 bg-black/70 border-b-1 border-neutral-700 items-center">
+                <img 
+                    src={img}
+                    alt="X Logo" 
+                    className="w-12 h-12 rounded-full p-0.5"
+                />
+                <h2>{content}</h2>
+                <span className='min-w-fit ml-auto'>{time}</span>
+            </div>
+    )
+  }
+
+  export default Notification
--- src/components/NotificationPage.tsx
+++ src/components/NotificationPage.tsx
@@ -1,6 +1,19 @@
-import logo from '../assets/LOGO_X.svg';
+
+import Notification from './Notification';
 
 const NotificationPage = () => {
+    const notificaation = [
+        {
+            user: '/src/assets/LOGO_X.svg',
+            content: 'Sua conta E-mail foi acessada de um novo dispositivo em 29 de jul. de 2025. Revise esse acesso.',
+            time: '30 de jul'
+        },
+        {
+            user: 'https://media.licdn.com/dms/image/v2/D4E03AQH6CzfgKuAXXw/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1699646804140?e=1757548800&v=beta&t=Vm1k6H39yAIon7sVUdBNLZ7OaXaKkTuZu08-aB7-17o',
+            content: 'Bruno Silva acabou de seguir você.',
+            time: '30 de jul'
+        },
+    ]
 
     return (
         <>
@@ -8,24 +21,15 @@ const NotificationPage = () => {
                 <div className="w-full py-5 bg-black/70 text-center place-items-center border-b-1 border-neutral-700  sticky top-0 left-0">
                     <h2>Notificações</h2>
                 </div>
-                <div className="flex w-full p-5 gap-4 bg-black/70  justify-between border-b-1 border-neutral-700">
-                    <img 
-                        src={logo} 
-                        alt="X Logo" 
-                        className="w-8"
+                
+                {notificaation.map((singleNotification, index) => (
+                    <Notification
+                        key={index}
+                        img={singleNotification.user}
+                        content={singleNotification.content}
+                        time={singleNotification.time}
                     />
-                    <h2>Sua conta E-mail foi acessada de um novo dispositivo em 29 de jul. de 2025. Revise esse acesso.</h2>
-                    <span className='min-w-fit'>30 de jul</span>
-                </div>
-                <div className="flex w-full p-5 gap-4 bg-black/70  justify-between border-b-1 border-neutral-700">
-                    <img 
-                        src={logo} 
-                        alt="X Logo" 
-                        className="w-8"
-                    />
-                    <h2>Sua conta E-mail foi acessada de um novo dispositivo em 29 de jul. de 2025. Revise esse acesso.</h2>
-                    <span className='min-w-fit'>30 de jul</span>
-                </div>
+                ))}
             </div>
         </>
     )
