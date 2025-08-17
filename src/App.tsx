646b2705fea40d10add3a7b093fccd17f050c953
Bruno Pereira da Silva <bunros52@gmail.com>
2025-08-17 00:07:14 -0300

    fix: fixing duplicate app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,15 @@
-<<<<<<< HEAD
+
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 import Login from "./pages/Login"
 import Home from "./pages/Home"
 import TweetList from './components/TweetList';
-=======
-import Home from "./pages/Home"
-import Login from "./pages/Login"
->>>>>>> 0cb8c4cb8becea8843841d74e9d699104a8c0f5c
 
 
 
 const App: React.FC = () => {
 
   return (
-<<<<<<< HEAD
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
@@ -22,11 +17,6 @@ const App: React.FC = () => {
         <Route path="/tweets" element={<TweetList />} />
       </Routes>
     </Router>
-=======
-    <>
-      <Login />
-    </>
->>>>>>> 0cb8c4cb8becea8843841d74e9d699104a8c0f5c
   )
 }
 
